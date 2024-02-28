17c30ef0373bacbbb1bc80d9398b54b92189cc8a
Zxl1556180489 <1556180489@qq.com>
2024-02-28 20:46:23 +0800

    “first”

--- model/DAHGCN.py
+++ model/DAHGCN.py
@@ -1,14 +1,14 @@
 from torch import nn
-from models import HGNN_conv
+from models import DAHGCN_conv
 import torch.nn.functional as F
 
 
-class HGNN(nn.Module):
+class DAHGCN(nn.Module):
     def __init__(self, in_ch, n_class, n_hid, dropout=0.5):
-        super(HGNN, self).__init__()
+        super(DAHGCN, self).__init__()
         self.dropout = dropout
-        self.hgc1 = HGNN_conv(in_ch, n_hid)
-        self.hgc2 = HGNN_conv(n_hid, n_class)
+        self.hgc1 = DAHGCN_conv(in_ch, n_hid)
+        self.hgc2 = DAHGCN_conv(n_hid, n_class)
 
     def forward(self, x, G):
         x = F.relu(self.hgc1(x, G))
